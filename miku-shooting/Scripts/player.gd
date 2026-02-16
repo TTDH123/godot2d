0c4a51250f307b547757c49c8986d512f01fc4d9
TTDH123 <117097387+TTDH123@users.noreply.github.com>
2026-02-16 17:03:40 +0900

    Add files via upload

---
+++ miku-shooting/Scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 500.0
+
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x := Input.get_axis("move_left", "move_right")
+	var direction_y := Input.get_axis("move_up", "move_down")
+	
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
