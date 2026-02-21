8d592157e833b39bee30bb8188c9cae11a391e92
TTDH123 <117097387+TTDH123@users.noreply.github.com>
2026-02-21 23:41:11 +0900

    Add files via upload

--- tutorial-platformer/Scripts/player.gd
+++ tutorial-platformer/Scripts/player.gd
@@ -1,21 +1,32 @@
 extends CharacterBody2D
 
-@export var movespeed: float = 300
-@export var jumpforce: float = -400
+@export var movespeed: float = 100
+@export var jumpforce: float = -300
 @onready var hud: CanvasLayer = $HUD
-
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var jump = 1
 var score: int:
 	set(new_value):
 		score = new_value
 		hud.set_score_text(score)
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
 	var dir = Input.get_axis("left", "right")
+	if dir != 0:
+		anim_sprite.flip_h = dir < 0
+	if not is_on_floor():
+		anim_sprite.play("jump")
+		velocity += get_gravity() * delta
+	else:
+		jump = 1
+		if dir == 0:
+			anim_sprite.play("idle")
+		else:
+			anim_sprite.play("run")
 	velocity.x = dir * movespeed
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+	if jump == 1 and Input.is_action_just_pressed("jump"):
+		if not is_on_floor():
+			jump = 0
 		velocity.y = jumpforce
 
 	move_and_slide()
